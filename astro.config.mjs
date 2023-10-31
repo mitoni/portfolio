d1f2caae103949c6b8632930fa593fc019aa9589
michele-tonizzo <mt@moi.am>
2023-10-31 09:24:51 +0100

    add sitemap automatic generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
         svelte(),
+        sitemap(),
     ],
+    site: "https://www.mitonizzo.com",
     scopedStyleStrategy: "where",
     image: {
         service: passthroughImageService()
